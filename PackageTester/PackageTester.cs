1079c053328bbd157996fbdfc83c64aadd5d479b
hwolf <hwolf@tegtmeier-inkubator.de>
2025-08-28 19:09:10 +0200

    upgrade PackageTester to v2.1.0 plus YAML example

--- PackageTester/PackageTester.cs
+++ PackageTester/PackageTester.cs
@@ -7,16 +7,33 @@ class Program
     static void Main()
     {
         // Sample JSON
-        string json = @"{
-            ""store"": {
-                ""book"": [
-                    { ""language"": ""German"", ""title"": ""Die Geschichte der Zukunft"", ""price"": 24.00 },
-                    { ""language"": ""English"", ""title"": ""The History of the Future"", ""price"": 16.24 }
-                ]
+        string json = """
+        {
+            "store": {
+            "books": [
+                { "language": "German", "title": "Die Geschichte der Zukunft", "price": 24.00 },
+                { "language": "English", "title": "The History of the Future", "price": 16.24 }
+            ]
             }
-        }";
+        }
+        """;
 
-        var node = JsonPath.Node.FromJson(json);
-        Console.WriteLine("store.book.language(English).price: " + node.AsDictionary["store"]["book"].AsList.FirstOrDefault(n => n["language"] == "English")?["price"].AsFloat);
+        var jsonNode = JsonPath.Node.FromJson(json);
+        Console.WriteLine("JSON: store.books.language(English).price: " + jsonNode.AsDictionary["store"]["books"].AsList.FirstOrDefault(n => n["language"] == "English")?["price"].AsFloat);
+
+        // Sample YAML
+        string yaml = """
+        store:
+          books:
+            - language: German
+              title: Die Geschichte der Zukunft
+              price: 24.00
+            - language: English
+              title: The History of the Future
+              price: 16.24
+        """;
+
+        var yamlNode = JsonPath.Node.FromYaml(yaml);
+        Console.WriteLine("YAML: store.books.language(English).price: " + yamlNode.AsDictionary["store"]["books"].AsList.FirstOrDefault(n => n["language"] == "English")?["price"].AsFloat);
     }
 }
